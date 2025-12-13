36741d9c74d5eb88bea99608d750d4d0ac322a03
GreenRabbit917 <nagelshlomo@gmail.com>
2025-12-13 11:22:40 -0500

    Got basic backend server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+    res.send(`Hello bradley!`);
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server listening on port ${PORT}`);
+});
